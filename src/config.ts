1d1e8e56ca3f1a14d2a98a5c81519a719eb2cebd
empsmileyalt <sanketpatelny@gmail.com>
2026-09-13 12:37:48 -0400

    Update server host and port in config

--- src/config.ts
+++ src/config.ts
@@ -46,8 +46,8 @@ export const config: Config = {
       originBlacklist: null,
     },
     server: {
-      host: "127.0.0.1",
-      port: 1111,
+      host: "89.117.77.192",
+      port: 25643,
     },
     tls: undefined,
   },
